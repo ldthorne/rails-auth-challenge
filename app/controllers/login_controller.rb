066a16215ed770d2665968ea807ad0c16feb64c4
Leon Thorne <ldthorne@brandeis.edu>
2016-10-26 19:19:33 -0400

    tabs to spaces

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,21 +1,21 @@
 class LoginController < ApplicationController
-	def index
-	end
+  def index
+  end
 
-	def create
-		user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+  def create
+    user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
 
-		if user
-			session[:uuid] = user.uuid
-			session[:email] = user.email
-			redirect_to '/'
-		else
-			redirect_to '/login'
-		end
-	end
+    if user
+      session[:uuid] = user.uuid
+      session[:email] = user.email
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
 
-	def destroy 
-		reset_session
-		redirect_to '/login'
-	end
+  def destroy 
+    reset_session
+    redirect_to '/login'
+  end
 end
--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,31 +1,31 @@
 class TaskController < ApplicationController
-	before_action :ensure_authenticated
+  before_action :ensure_authenticated
 
-	def index
-		user = User.find_by uuid: session[:uuid]
-		@tasks = Task.where(user_id: user.id)
-	end
+  def index
+    user = User.find_by uuid: session[:uuid]
+    @tasks = Task.where(user_id: user.id)
+  end
 
-	def create
-		user = User.find_by uuid: session[:uuid]
-		due_date = Date.parse(params[:due_date])
-		Task.create!(due_date: due_date, description: params[:description], user_id: user.id)
-		redirect_to '/'
-	end
+  def create
+    user = User.find_by uuid: session[:uuid]
+    due_date = Date.parse(params[:due_date])
+    Task.create!(due_date: due_date, description: params[:description], user_id: user.id)
+    redirect_to '/'
+  end
 
-	def update
-		task = Task.find(params[:id])
-		task.is_complete = !task.is_complete
-		task.save!
-		redirect_to '/'
-	end
+  def update
+    task = Task.find(params[:id])
+    task.is_complete = !task.is_complete
+    task.save!
+    redirect_to '/'
+  end
 
-	private
+  private
 
-	def ensure_authenticated
-		user = User.find_by uuid: session[:uuid]
-		if !user.present?
-			redirect_to '/login'
-		end
-	end
+  def ensure_authenticated
+    user = User.find_by uuid: session[:uuid]
+    if !user.present?
+      redirect_to '/login'
+    end
+  end
 end
